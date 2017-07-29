f44089bdece385ee1a3571c90b2819ab3fa281f4
Alex Forrence <aforren1@jhu.edu>
2017-07-29 07:41:31 -0400

    notes on hiding/showing cursor

--- input/keyboard.py
+++ input/keyboard.py
@@ -1,4 +1,11 @@
 from input.input_base import InputBase
 import keyboard
+from ctypes import windll
+
+user32 = windll.user32
+
+user32.ShowCursor(False)
+
+user32.ShowCursor(True)
 
 # http://www.psychopy.org/api/iohub/device/keyboard.html
