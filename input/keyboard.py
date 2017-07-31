fd293798159e191b6b29a412bc8ff151a0da2c96
Alex Forrence <aforren1@jhu.edu>
2017-07-31 10:00:47 -0400

    Rethink keyboard and such

--- input/keyboard.py
+++ input/keyboard.py
@@ -1,11 +1,27 @@
-from input.input_base import InputBase
+from input.base_input import BaseInput
 import keyboard
-from ctypes import windll
-
-user32 = windll.user32
-
-user32.ShowCursor(False)
-
-user32.ShowCursor(True)
 
 # http://www.psychopy.org/api/iohub/device/keyboard.html
+
+class Keyboard(BaseInput):
+    def __init__(self, *args, **kwargs):
+        super(Keyboard, self).__init__(*args, **kwargs)
+        self.down_callbacks = []
+        self.up_callbacks = []
+    
+    def start(self, keys, down_callbacks, up_callbacks):
+        super(Keyboard, self).start()
+        
+    
+    def stop(self):
+        return
+
+    def read(self):
+        return super(Keyboard, self).read()
+    
+    def _world_to_raw(self):
+        return 1
+
+    def clear(self):
+        return
+
