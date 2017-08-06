f885f308f2c1da92b9cc63b1f9a23e37720c5c78
Alex Forrence <aforren1@jhu.edu>
2017-08-06 17:32:55 -0400

    Messy, but working, example of sharing a numpy array

--- input/kata_hand.py
+++ input/kata_hand.py
@@ -3,30 +3,51 @@ import multiprocessing as mp
 import struct
 import numpy as np
 import hid
+import psychopy.core
 
 def shared_to_numpy(mp_arr, nrow, ncol):
     return np.frombuffer(mp_arr.get_obj()).reshape((nrow,ncol))
 
-def worker(remote_buffer_):
-    global remote_buffer
-    remote_buffer = remote_buffer_
-    while True:
-        with remote_buffer_.get_lock():
-            arr = shared_to_numpy(remote_buffer)
+def worker(remote_buffer, poison_pill, nrow, ncol, clock, wait):
+    # set poison_pill.value to False to end worker
+    while poison_pill.value:
+        with remote_buffer.get_lock():
+            # convert to numpy array for more friendly modifications
+            arr = shared_to_numpy(remote_buffer, nrow, ncol)
             # get first non-nan row
-            bytes_from_dev = d.read(64)
-            arr[i] = struct.unpack('>LHH', bytes_from_dev)
+            #bytes_from_dev = d.read(64)
+            #arr[i] = struct.unpack('>LHH', bytes_from_dev)
+            current_nans = np.isnan(arr).any(axis=1)
+            if current_nans.any():
+                next_index = np.where(current_nans)[0][0]
+                arr[next_index, 0] = clock()
+                arr[next_index, 1] = np.random.random()
+            else:
+                arr[:] = np.roll(arr, -1, axis=0)
+                arr[-1,0] = clock()
+                arr[-1,1] = np.random.random()
+            wait(1)
 
 
-nrow = 5
-ncol = 11
-remote_buffer = mp.Array(ctypes.c_double, nrow*ncol)
+if __name__=='__main__':
+    nrow = 10
+    ncol = 2
+    remote_buffer = mp.Array(ctypes.c_double, nrow*ncol)
 
-arr = shared_to_numpy(remote_buffer, nrow, ncol)
-arr[:] = np.nan
+    arr = shared_to_numpy(remote_buffer, nrow, ncol)
+    arr.fill(np.nan)
 
-p = Process(target=worker, args=(remote_buffer,))
+    poison_pill = mp.Value(ctypes.c_bool)
+    poison_pill.value = True
+    clock = psychopy.core.getTime
+    wait = psychopy.core.wait
+
+    p = mp.Process(target=worker, args=(remote_buffer, poison_pill, nrow, ncol, clock, wait))
+    p.start()
+    wait(14)
+
+    result = shared_to_numpy(remote_buffer, nrow, ncol)
+    print(result)
+    poison_pill.value = False
 
 # get the numpy array out as
-result = shared_to_numpy(remote_buffer, nrow, ncol)
-
