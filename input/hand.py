ab72bf1e5eca042110ef42bd84bd005e95ed82f6
Alex Forrence <aforren1@jhu.edu>
2017-08-07 11:08:27 -0400

    Add docs, default to no multiprocessing

--- input/hand.py
+++ input/hand.py
@@ -8,9 +8,32 @@ import psychopy.core
 def shared_to_numpy(mp_arr, nrow, ncol):
     return np.frombuffer(mp_arr.get_obj()).reshape((nrow,ncol))
 
-
 class Hand(object):
-    def __init__(self, buffer_rows=50, multiproc=True):
+    """
+    A class that handles communication with HAND.
+
+    Example usage:
+
+    from hand import Hand
+    dev = Hand(multiproc=True)
+    
+    dev.start()
+
+    data = dev.read() # returns buffer since last read
+
+    dev.stop() # stop device
+
+    dev.start() # re-open device
+
+    dev.close() # also calls dev.stop()
+
+    """
+    def __init__(self, buffer_rows=50, multiproc=False):
+        """
+        If multiproc is True, sets up remote interface for polling the device.
+        The size of the shared buffer can be set via buffer_rows.
+
+        """
         self.ncol = 16
         self.nrow = buffer_rows
         self._device = None
@@ -27,6 +50,9 @@ class Hand(object):
             self.poison_pill.value = True
 
     def start(self):
+        """
+        If multiproc is True, start the remote process. Otherwise, open the HID communication.
+        """
         if self.multiproc:
             self.poison_pill.value = True
             self.clear()
@@ -40,23 +66,40 @@ class Hand(object):
             self._device.set_nonblocking(1)
     
     def stop(self):
+        """
+        If multiproc is True, stop the remote process (does nothing otherwise).
+        """
         if self.multiproc:
             self.poison_pill.value = False
     
     def close(self):
+        """
+        Close the HID interface.
+        """
         self.stop()
         if not self.multiproc:
             self._device.close()
     
     def read(self):
+        """
+        Returns a single reading (multiproc=False) or the all values stored
+        in the shared buffer (multiproc=True).
+
+        If no data, returns None (multiproc=False and True).
+        """
         # TODO: return both x,y,z (based on median) AND raw data
         if self.multiproc:
             np.copyto(self.read_buffer, self.shared_buffer)
             self.clear()
-            return(self.read_buffer)
+            if np.isnan(self.read_buffer).all():
+                return None
+            return(self.read_buffer[~np.isnan(self.read_buffer).any(axis=1)])
         return self._read()
     
     def _read(self):
+        """
+        Core read function. Please use read(), which abstracts away the multiprocessing parts.
+        """
         data = self._device.read(46)
         if data:
             data = struct.unpack('>LhHHHHHHHHHHHHHHHHHHHH', bytearray(data))
@@ -71,15 +114,27 @@ class Hand(object):
         return(None)
 
     def write(self):
+        """
+        Write to device. Will be used to set sampling frequency, amplifier gains, etc.
+        """
         raise NotImplementedError('Alex needs to implement this.')
     
     def clear(self):
+        """
+        Clear the shared buffer.
+        """
         self.shared_buffer.fill(np.nan)
     
     def worker(self, shared_buffer, poison_pill, nrow, ncol):
+        """
+        Workhorse for polling the device on a remote process.
+        """
         self._device = hid.device()
         self._device.open(0x16c0, 0x486)
         self._device.set_nonblocking(1)
+        # (try to) clear buffer
+        for i in range(50):
+            notused = self.read()
         while poison_pill.value:
             data = self._read()
             if data is not None:
