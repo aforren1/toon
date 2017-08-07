52a7103be346a8d19050476b12d2a5e666188378
Alex Forrence <aforren1@jhu.edu>
2017-08-06 22:03:40 -0400

    Demonstrate HAND (minus device functionality, which will come from plotosaurus)

--- input/kata_hand.py
+++ input/kata_hand.py
@@ -8,46 +8,60 @@ import psychopy.core
 def shared_to_numpy(mp_arr, nrow, ncol):
     return np.frombuffer(mp_arr.get_obj()).reshape((nrow,ncol))
 
-def worker(remote_buffer, poison_pill, nrow, ncol, clock, wait):
-    # set poison_pill.value to False to end worker
-    while poison_pill.value:
-        with remote_buffer.get_lock():
-            # convert to numpy array for more friendly modifications
-            arr = shared_to_numpy(remote_buffer, nrow, ncol)
-            # get first non-nan row
-            #bytes_from_dev = d.read(64)
-            #arr[i] = struct.unpack('>LHH', bytes_from_dev)
-            current_nans = np.isnan(arr).any(axis=1)
-            if current_nans.any():
-                next_index = np.where(current_nans)[0][0]
-                arr[next_index, 0] = clock()
-                arr[next_index, 1] = np.random.random()
-            else:
-                arr[:] = np.roll(arr, -1, axis=0)
-                arr[-1,0] = clock()
-                arr[-1,1] = np.random.random()
-            wait(1)
 
+class KataHand(object):
+    def __init__(self, buffer_rows=50):
+        self.ncol = 2
+        self.nrow = buffer_rows
+        self._shared_buffer = mp.Array(ctypes.c_double, self.nrow*self.ncol)
+        self.shared_buffer = shared_to_numpy(self._shared_buffer, self.nrow, self.ncol)
+        self.shared_buffer.fill(np.nan)
+
+        self.poison_pill = mp.Value(ctypes.c_bool)
+        self.poison_pill.value = True
+
+    def start(self):
+        self.poison_pill.value = True
+        self.clear()
+        self.process = mp.Process(target=self.worker,
+                                  args=(self._shared_buffer, self.poison_pill,
+                                        self.nrow, self.ncol))
+        self.process.start()
+    
+    def stop(self):
+        self.poison_pill.value = False
+    
+    def read(self):
+        res = np.copy(self.shared_buffer)
+        self.clear()
+        return(res)
+
+    def write(self):
+        raise NotImplementedError('Alex needs to implement this.')
+    
+    def clear(self):
+        self.shared_buffer.fill(np.nan)
+    
+    def worker(self, shared_buffer, poison_pill, nrow, ncol):
+        while poison_pill.value:
+            with shared_buffer.get_lock():
+                arr = shared_to_numpy(shared_buffer, nrow, ncol)
+                current_nans = np.isnan(arr).any(axis=1)
+                if current_nans.any():
+                    next_index = np.where(current_nans)[0][0]
+                    arr[next_index,0] = psychopy.core.getTime()
+                    arr[next_index,1] = np.random.random()
+                else:
+                    arr[:] = np.roll(arr, -1, axis=0)
+                    arr[-1,0] = psychopy.core.getTime()
+                    arr[-1,1] = np.random.random()
+                psychopy.core.wait(0.0001)
 
 if __name__=='__main__':
-    nrow = 10
-    ncol = 2
-    remote_buffer = mp.Array(ctypes.c_double, nrow*ncol)
+    hd = KataHand(buffer_rows=100)
 
-    arr = shared_to_numpy(remote_buffer, nrow, ncol)
-    arr.fill(np.nan)
-
-    poison_pill = mp.Value(ctypes.c_bool)
-    poison_pill.value = True
-    clock = psychopy.core.getTime
-    wait = psychopy.core.wait
-
-    p = mp.Process(target=worker, args=(remote_buffer, poison_pill, nrow, ncol, clock, wait))
-    p.start()
-    wait(14)
-
-    result = shared_to_numpy(remote_buffer, nrow, ncol)
-    print(result)
-    poison_pill.value = False
-
-# get the numpy array out as
+    hd.start()
+    for ii in range(50):
+        print(hd.read())
+        psychopy.core.wait(0.16)
+    hd.stop()
