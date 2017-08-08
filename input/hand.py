be40663f3543008f732ca79ccae865a772728408
Alex Forrence <aforren1@jhu.edu>
2017-08-08 12:40:47 -0400

    Simple example, formatting, few bug fixes

--- input/hand.py
+++ input/hand.py
@@ -3,10 +3,11 @@ import multiprocessing as mp
 import struct
 import numpy as np
 import hid
-import psychopy.core
+
 
 def shared_to_numpy(mp_arr, nrow, ncol):
-    return np.frombuffer(mp_arr.get_obj()).reshape((nrow,ncol))
+    return np.frombuffer(mp_arr.get_obj()).reshape((nrow, ncol))
+
 
 class Hand(object):
     """
@@ -23,25 +24,28 @@ class Hand(object):
     dev.start() # re-open device
     dev.close() # also calls dev.stop()
     """
-    def __init__(self, buffer_rows=50, multiproc=False):
+
+    def __init__(self, time, buffer_rows=50, multiproc=False):
         """
         If multiproc is True, sets up remote interface for polling the device.
         The size of the shared buffer can be set via buffer_rows.
         """
-        self.ncol = 16
+        self.ncol = 17
         self.nrow = buffer_rows
         self._device = None
         self.multiproc = multiproc
-        self._force_data = np.full(17, np.nan)
+        self._force_data = np.full(self.ncol, np.nan)
         self._rot_val = np.pi / 4.0
+        self.monotime = time
 
         if multiproc:
-            self._shared_buffer = mp.Array(ctypes.c_double, self.nrow*self.ncol)
+            self._shared_buffer = mp.Array(ctypes.c_double, self.nrow * self.ncol)
             self.shared_buffer = shared_to_numpy(self._shared_buffer, self.nrow, self.ncol)
             self.shared_buffer.fill(np.nan)
             self.read_buffer = np.full((self.nrow, self.ncol), np.nan)
             self.poison_pill = mp.Value(ctypes.c_bool)
             self.poison_pill.value = True
+            self.process = None
 
     def start(self):
         """
@@ -52,25 +56,25 @@ class Hand(object):
             self.poison_pill.value = True
             self.clear()
             self.process = mp.Process(target=self.worker,
-                                      args=(self._shared_buffer, self.poison_pill,
-                                            self.nrow, self.ncol))
+                                      args=(self._shared_buffer, self.poison_pill))
+            self.process.daemon = True
             self.process.start()
         else:
             self._device = hid.device()
             self._device.open(0x16c0, 0x486)
             self._device.set_nonblocking(1)
-    
+
     def stop(self):
         """ If multiproc is True, stop the remote process (does nothing otherwise)."""
         if self.multiproc:
             self.poison_pill.value = False
-    
+
     def close(self):
         """ Close the HID interface."""
         self.stop()
         if not self.multiproc:
             self._device.close()
-    
+
     def read(self):
         """
         Returns a single reading (multiproc=False) or the all values stored
@@ -83,9 +87,9 @@ class Hand(object):
             self.clear()
             if np.isnan(self.read_buffer).all():
                 return None
-            return(self.read_buffer[~np.isnan(self.read_buffer).any(axis=1)])
+            return self.read_buffer[~np.isnan(self.read_buffer).any(axis=1)]
         return self._read()
-    
+
     def _read(self):
         """ Core read function. Please use read(), which abstracts away the multiprocessing parts."""
         data = self._device.read(46)
@@ -94,30 +98,30 @@ class Hand(object):
             data = np.asarray(data, dtype='d')
             data[0] /= 1000.0
             data[1:] /= 65535.0
-            self._force_data[0] = psychopy.core.getAbsTime() # game time
-            self._force_data[1] = data[0] # HAND's personal time
-            self._force_data[1::3] = data[2::4] * np.cos(self._rot_val) - data[3::4] * np.sin(self._rot_val) # x
-            self._force_data[2::3] = data[2::4] * np.sin(self._rot_val) + data[3::4] * np.cos(self._rot_val) # y
-            self._force_data[3::3] = data[4::4] + data[5::4] # z
-            return(self._force_data)
-        return(None)
+            self._force_data[0] = self.monotime.getTime()  # game time
+            self._force_data[1] = data[0]  # HAND's personal time
+            self._force_data[2::3] = data[2::4] * np.cos(self._rot_val) - data[3::4] * np.sin(self._rot_val)  # x
+            self._force_data[3::3] = data[2::4] * np.sin(self._rot_val) + data[3::4] * np.cos(self._rot_val)  # y
+            self._force_data[4::3] = data[4::4] + data[5::4]  # z
+            return self._force_data
+        return None
 
     def write(self):
         """ Write to device. Will be used to set sampling frequency, amplifier gains, etc."""
         raise NotImplementedError('Alex needs to implement this.')
-    
+
     def clear(self):
         """ Clear the shared buffer. """
-        with self.shared_buffer.get_lock():
+        with self._shared_buffer.get_lock():
             self.shared_buffer.fill(np.nan)
-    
-    def worker(self, shared_buffer, poison_pill, nrow, ncol):
+
+    def worker(self, shared_buffer, poison_pill):
         """ Workhorse for polling the device on a remote process."""
         self._device = hid.device()
         self._device.open(0x16c0, 0x486)
         self._device.set_nonblocking(1)
         # (try to) clear buffer
-        arr = shared_to_numpy(shared_buffer, nrow, ncol)
+        arr = shared_to_numpy(shared_buffer, self.nrow, self.ncol)
         for i in range(50):
             self.read()
         while poison_pill.value:
@@ -127,8 +131,25 @@ class Hand(object):
                     current_nans = np.isnan(arr).any(axis=1)
                     if current_nans.any():
                         next_index = np.where(current_nans)[0][0]
-                        arr[next_index,:] = data
+                        arr[next_index, :] = data
                     else:
                         arr[:] = np.roll(arr, -1, axis=0)
-                        arr[next_index,:] = data
+                        arr[-1, :] = data
         self._device.close()
+
+
+if __name__ == '__main__':
+    import psychopy.core
+
+    monotime = psychopy.core.monotonicClock
+    dev = Hand(time=monotime, multiproc=True)
+    dev.start()
+    psychopy.core.wait(0.016, hogCPUperiod=0.016)
+    data = dev.read()
+    print(data[:,0])
+    print(data[:,1])
+    psychopy.core.wait(0.016, hogCPUperiod=0.016)
+    data = dev.read()
+    print(data[:,0])
+    print(data[:,1])
+    dev.close()
