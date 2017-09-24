e8689e270a88808c31be8d9ae963bf352605dca0
Alex Forrence <aforren1@jhu.edu>
2017-09-24 15:58:17 -0400

    Improve mouse compatibility, adjust kinereach example

--- toon/examples/kinereach.py
+++ toon/examples/kinereach.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     freeze_support()
     import numpy as np
     from psychopy import core, visual, event, monitors
-    from toon.input import BlamBirds
+    from toon.input import BlamBirds, Mouse
 
     flock=False
     mon = monitors.Monitor('tmp')
@@ -15,13 +15,15 @@ if __name__ == '__main__':
                         screen=1, monitor=mon, units='cm',
                         allowGUI=False)
 
-    win.viewScale = [-1, 1]  # mirror image
 
-
-    device = BlamBirds(multiprocess=True, master='/dev/ttyUSB0',
-                       ports=['/dev/ttyUSB0', '/dev/ttyUSB1'])
-
+    if flock:
+        device = BlamBirds(multiprocess=True, master='/dev/ttyUSB0',
+                           ports=['/dev/ttyUSB0', '/dev/ttyUSB1'])
+        win.viewScale = [-1, 1]  # mirror image
+    else:
+        device = Mouse(win=win)
     core.wait(1)
+
     with device as dev:
         center = visual.Circle(win, radius=2, fillColor='green', pos=(0, 0),
                                autoDraw=True)
@@ -29,22 +31,16 @@ if __name__ == '__main__':
         pointer = visual.Circle(win, radius=2.54/2, fillColor='darkmagenta',
                                 pos=(0, 0), autoDraw=True)
 
-        if flock:
-            baseline = None
-            while baseline is None:
-                baseline = dev.read()[0]
-            baseline = np.median(baseline, axis=0)[0:2]
-        else:
-            baseline = dev.getPos()
+        baseline = None
+        while baseline is None:
+            baseline = dev.read()[0]
+        baseline = np.median(baseline, axis=0)[0:2]
         pointer.pos = baseline
 
         while not event.getKeys():
-            if flock:
-                data = dev.read()[0]
-                if data is not None:
-                    newdata = np.median(data, axis=0)[0:2]
-                    print(newdata)
-                    pointer.pos = newdata
-            else:
-                pointer.pos = dev.getPos()
+            data = dev.read()[0]
+            if data is not None:
+                newdata = data[-1, 0:2]
+                print(newdata)
+                pointer.pos = newdata
             win.flip()
--- toon/input/mouse.py
+++ toon/input/mouse.py
@@ -3,7 +3,7 @@ Temporary mouse class (for compat with our input devices)
 
 Probably not ideal for experiments
 """
-
+import numpy as np
 from psychopy import core, event
 
 class Mouse(object):
@@ -12,12 +12,15 @@ class Mouse(object):
         self.time = clock_source
         self.multiprocess = multiprocess
         self._mouse = event.Mouse(visible=False, win=win)
+        self._array = np.array([[3.0,2.1]])
 
     def __enter__(self):
         self._start_time = self.time.getTime()
+        return self
 
     def read(self):
-        return self._mouse.getPos(), self.time.getTime()
+        self._array[0][0:2] = self._mouse.getPos()
+        return self._array, self.time.getTime()
 
     def __exit__(self, type, value, traceback):
         pass
