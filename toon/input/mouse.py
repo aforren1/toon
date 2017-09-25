a40eea9355e70398982a726a9befa1d6180deaae
Alex Forrence <aforren1@jhu.edu>
2017-09-24 20:58:24 -0400

    Tweak the mouse

--- toon/input/mouse.py
+++ toon/input/mouse.py
@@ -4,14 +4,16 @@ Temporary mouse class (for compat with our input devices)
 Probably not ideal for experiments
 """
 import numpy as np
-from psychopy import core, event
+from psychopy import event
+from toon.input.base_input import DummyTime
 
 class Mouse(object):
-    def __init__(self, clock_source=core.monotonicClock,
+    def __init__(self, clock_source=DummyTime,
                  multiprocess=False, win=None):
         self.time = clock_source
         self.multiprocess = multiprocess
-        self._mouse = event.Mouse(visible=False, win=win)
+        self.win = win
+        self._mouse = event.Mouse(win=win)
         self._array = np.array([[3.0,2.1]])
 
     def __enter__(self):
