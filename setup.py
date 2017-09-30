79b984876889c3b3ba63fd2ab1dffba551ae31cc
Alex Forrence <aforren1@jhu.edu>
2017-09-30 09:23:30 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='toon',
-    version='0.3.0',
+    version='0.3.5',
     description='Tools for neuroscience experiments',
     long_description=long_description,
     url='https://github.com/aforren1/toon',
