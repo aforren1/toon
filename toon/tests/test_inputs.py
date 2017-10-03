e88b7b6cde96b10866d1fc0c58a558ae1ca56ad8
Alex Forrence <aforren1@jhu.edu>
2017-10-03 14:46:59 -0400

    Interactive input testing

--- toon/tests/test_inputs.py
+++ toon/tests/test_inputs.py
@@ -1,16 +1,43 @@
-# #from toon.input import Keyboard as kb
-# from toon.input import Mouse as ms
-# from time import time, sleep
-#
-# #dev = kb(multiprocess=True, keys = ['a', 's', 'd', 'f'])
-# dev = ms(multiprocess=True)
-#
-# if __name__=='__main__':
-#     with dev as d:
-#         t0 = time()
-#         t1 = t0 + 20
-#         while time() < t1:
-#             data, timestamp = d.read()
-#             if data is not None:
-#                 print((data, timestamp - t0))
-#             sleep(0.016)
+from time import sleep
+import sys
+from toon.input import Keyboard, Hand, BlamBirds, DebugKeyboard, ForceTransducers
+from psychopy import core
+import numpy as np
+
+np.set_printoptions(precision=4, suppress=True)
+
+if __name__=='__main__':
+
+    device = str(sys.argv[1])
+    mp = bool(sys.argv[2])
+
+    timer = core.monotonicClock
+
+    if device == 'keyboard':
+        dev = Keyboard(multiprocess=mp, keys=['a', 's', 'd', 'f'], clock_source=timer)
+    elif device == 'hand':
+        dev = Hand(multiprocess=mp, clock_source=timer)
+    elif device == 'birds':
+        dev = BlamBirds(multiprocess=mp, ports=['COM11', 'COM12', 'COM13', 'COM14'],
+                        master='COM11', sample_ports=['COM11', 'COM13'],
+                        clock_source=timer)
+    elif device == 'dbkeyboard':
+        dev = DebugKeyboard(multiprocess=mp, keys=['a', 's', 'd', 'f'], clock_source=timer)
+    elif device == 'forcetransducers':
+        dev = ForceTransducers(multiprocess=mp, clock_source=timer)
+    else:
+        print('Pass the device as the first arg, and True/False as the second (for multiprocessing)')
+        print("Available devices are: 'keyboard', 'hand', 'birds', 'dbkeyboard', 'forcetransducers'")
+        sys.exit()
+
+    with dev as d:
+        t0 = timer.getTime()
+        t1 = t0 + 10
+        while timer.getTime() < t1:
+            timestamp, data = d.read()
+            if data is not None:
+                print(timestamp - t0)
+                #print(data)
+            sleep(0.016)
+
+    sys.exit()
