bfedd234397b62e6734056691448b11d4051ed53
Alex Forrence <aforren1@jhu.edu>
2017-10-16 15:22:51 -0400

    Make sure it works on Travis

--- toon/tests/test_input2.py
+++ toon/tests/test_input2.py
@@ -1,7 +1,12 @@
 import numpy as np
 from toon.tests.fake_class import FakeInput
 from toon.input.base_input2 import Input
-from psychopy.clock import monotonicClock
+import os
+if 'TRAVIS' not in os.environ:
+    from psychopy.clock import monotonicClock
+    time = monotonicClock.getTime
+else:
+    from time import time
 
 np.set_printoptions(precision=4)
 
@@ -11,39 +16,39 @@ single_data = Input(FakeInput,
                     mp=False,
                     data_dims=5,
                     read_delay=0.001,
-                    clock_source=mt.getTime)
+                    clock_source=time)
 
 multi_data = Input(FakeInput,
                    mp=False,
                    data_dims=[[5], [3, 2]],
-                   clock_source=mt.getTime,
+                   clock_source=time,
                    read_delay=0.001)
 
 single_mp = Input(FakeInput,
                   mp=True,
                   data_dims=5,
                   read_delay=0.001,
-                  clock_source=mt.getTime)
+                  clock_source=time)
 
 multi_mp = Input(FakeInput,
                  mp=True,
                  data_dims=[[5], [3, 2]],
-                 clock_source=mt.getTime,
+                 clock_source=time,
                  read_delay=0.001)
 
 
 def read_fn(dev):
     with dev as d:
-        t0 = mt.getTime()
+        t0 = time()
         t1 = t0 + 5
-        while t1 > mt.getTime():
-            t2 = mt.getTime()
+        while t1 > time():
+            t2 = time()
             t3 = 0.016 + t2
             timestamps, data = d.read()
             if timestamps is not None:
                 print(timestamps - t0)
                 print(data)
-            while t3 > mt.getTime():
+            while t3 > time():
                 pass
 
 
