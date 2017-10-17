d68f6b07eca063076de6d156b6ac236795972c1f
Alex Forrence <aforren1@jhu.edu>
2017-10-17 16:05:37 -0400

    Rename demo input, remove old file

--- toon/examples/try_inputs.py
+++ toon/examples/try_inputs.py
@@ -3,13 +3,13 @@ import sys
 from distutils import util
 import argparse
 from platform import system
-from toon.input import Keyboard, Hand, BlamBirds
+from toon.input import Keyboard, Hand, BlamBirds, MultiprocessInput
 import numpy as np
 if system() is 'Windows':
     from toon.input import ForceTransducers
 
 # Call via
-# python -m toon.examples.test_inputs --dev keyboard --mp True
+# python -m toon.examples.try_inputs --dev keyboard --mp True
 import os
 not_travis = 'TRAVIS' not in os.environ
 if not_travis:
@@ -34,22 +34,27 @@ if __name__=='__main__':
     else:
         from time import time
     if device == 'keyboard':
-        dev = Keyboard(multiprocess=mp, keys=['a', 's', 'd', 'f'], clock_source=time)
+        dev = Keyboard(keys=['a', 's', 'd', 'f'], clock_source=time)
     elif device == 'hand':
-        dev = Hand(multiprocess=mp, clock_source=time)
+        dev = Hand(clock_source=time)
     elif device == 'birds':
         # settings for my laptop
-        dev = BlamBirds(multiprocess=mp, ports=['COM11', 'COM12', 'COM13', 'COM10'],
-                        master='COM11', sample_ports=['COM11', 'COM13'],
+        dev = BlamBirds(ports=['COM11', 'COM12', 'COM13', 'COM10'],
+                        master='COM11',
+                        sample_ports=['COM11', 'COM13'],
                         clock_source=time)
     elif device == 'forcetransducers':
-        dev = ForceTransducers(multiprocess=mp, clock_source=time)
+        dev = ForceTransducers(clock_source=time)
     else:
         print('Pass the device as the first arg, and True/False as the second (for multiprocessing)')
         print("Available devices are: 'keyboard', 'hand', 'birds', 'forcetransducers'")
         sys.exit()
+    if mp:
+        device = MultiprocessInput(dev)
+    else:
+        device = dev
 
-    with dev as d:
+    with device as d:
         t0 = time()
         t1 = t0 + 10
         while time() < t1:
