537fdc20154bbb1225a8c2428784ae40d7b704bb
Alex Forrence <aforren1@jhu.edu>
2017-10-18 10:47:03 -0400

    Switch to multiprocessing Events, stop locking the pipe

--- toon/input/mp_piper.py
+++ toon/input/mp_piper.py
@@ -1,45 +1,36 @@
 import multiprocessing as mp
-import ctypes
 
-class MultiprocessTest(object):
+class MultiprocessPiper(object):
     def __init__(self, device=None, _sampling_period=0):
         self._device = device
         self.local, self.remote = mp.Pipe(duplex=False)
-        self.lock = mp.RLock()
-        self.poison_pill = mp.Value(ctypes.c_bool)
+        self.remote_ready = mp.Event()
+        self.stop_remote = mp.Event()
         self._sampling_period = _sampling_period
 
-        # write to remote, read in local
-        # oldest events read first
-
     def __enter__(self):
-        self.poison_pill.value = False
         self._process = mp.Process(target=self._mp_worker,
                                    args=(self.remote,
-                                         self.lock,
-                                         self.poison_pill))
+                                         self.remote_ready,
+                                         self.stop_remote))
         self._process.daemon = True
         self._clear_pipe()
         self._process.start()
+        self.remote_ready.wait()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        with self.poison_pill.get_lock():
-            self.poison_pill.value = True
-        #self._process.join()
+        self.stop_remote.set()
 
-    def _mp_worker(self, remote, lock, poison_pill):
+    def _mp_worker(self, remote, remote_ready, stop_remote):
         with self._device as dev:
-            stop_sampling = False
-            while not stop_sampling:
+            remote_ready.set()
+            while not stop_remote.is_set():
                 t0 = dev.time()
-                with poison_pill.get_lock():
-                    stop_sampling = poison_pill.value
                 timestamp, data = dev.read()
                 if timestamp is not None:
-                    with lock:
-                        remote.send({'time': timestamp,
-                                     'data': data})
+                    remote.send({'time': timestamp,
+                                 'data': data})
                     while self._sampling_period > (dev.time() - t0):
                         pass
 
@@ -49,9 +40,8 @@ class MultiprocessTest(object):
 
     def read(self):
         data = list()
-        with self.lock:
-            while self.local.poll():
-                data.append(self.local.recv())
+        while self.local.poll():
+            data.append(self.local.recv())
         if len(data) == 0:
             return None
         return data
