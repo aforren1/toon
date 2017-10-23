307675d80e22fe4ca922bb98d45e79c2e8bfd82a
Alex Forrence <aforren1@jhu.edu>
2017-10-23 18:18:39 -0400

    Play with priority, garbage collection

--- toon/input/mp_input.py
+++ toon/input/mp_input.py
@@ -1,8 +1,10 @@
 import multiprocessing as mp
+import gc
 import psutil
 
 class MultiprocessInput(object):
-    def __init__(self, device=None, _sampling_period=0):
+    def __init__(self, device=None, _sampling_period=0,
+                 disable_gc=False, priority='normal'):
         """
         Allows the user poll an input device without blocking the main process.
         Args:
@@ -15,6 +17,8 @@ class MultiprocessInput(object):
         self.remote_ready = mp.Event()
         self.stop_remote = mp.Event()
         self._sampling_period = _sampling_period
+        self._disable_gc = disable_gc
+        self._priority = priority
 
     def __enter__(self):
         self.remote_ready.clear()
@@ -27,16 +31,24 @@ class MultiprocessInput(object):
         self._clear_pipe()
         self._process.start()
         self.remote_ready.wait()
-        parent = psutil.Process()
-        for child in parent.children():
-            if psutil.WINDOWS:
-                child.nice(psutil.HIGH_PRIORITY_CLASS)
-            else:
-                pass # TODO: handle niceness on Unix-like OSes
+        self._pid = self._process.pid
+        self._proc = psutil.Process(self._pid)
+        self._original_nice = self._proc.nice()
+        self.set_priority('high')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        self.set_priority('norm')
         self.stop_remote.set()
+
+    def read(self):
+        """Read all pending data from the 'receiving' end of the multiprocessing Pipe."""
+        data = list()
+        while self.local.poll():
+            data.append(self.local.recv())
+        if data:
+            return data
+        return None
 
     def _mp_worker(self, remote, remote_ready, stop_remote):
         """
@@ -47,6 +59,8 @@ class MultiprocessInput(object):
             stop_remote: Used to tell the remote process to stop sampling.
 
         """
+        if self._disable_gc:
+            gc.disable()
         with self._device as dev:
             remote_ready.set()
             t0 = dev.time() + self._sampling_period  # first sampling period will be off
@@ -63,11 +77,15 @@ class MultiprocessInput(object):
         while self.local.poll():
             self.local.recv()
 
-    def read(self):
-        """Read all pending data from the 'receiving' end of the multiprocessing Pipe."""
-        data = list()
-        while self.local.poll():
-            data.append(self.local.recv())
-        if data:
-            return data
-        return None
+    def set_priority(self, val):
+
+        try:
+            if val == 'high':
+                if psutil.WINDOWS:
+                    self._proc.nice(psutil.HIGH_PRIORITY_CLASS)
+                else:
+                    self._proc.nice(-10)
+            else:
+                self._proc.nice(self._original_nice)
+        except psutil.AccessDenied:
+            pass
