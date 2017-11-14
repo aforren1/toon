d1dba28c4b3936d1a3208abfba54e8e0e7dc1aa2
Alex Forrence <aforren1@jhu.edu>
2017-11-14 18:31:46 -0500

    First pass at converting force transducers

--- toon/input/force_transducers.py
+++ toon/input/force_transducers.py
@@ -1,52 +1,49 @@
-import platform
 import numpy as np
-from toon.input.base_input import BaseInput
+from toon.input.lsl_wrapper import BaseInput
 
-if platform.system() is 'Windows':
-    import nidaqmx
-    import nidaqmx.system
-    from nidaqmx.constants import AcquisitionType, TerminalConfiguration
-    from nidaqmx.stream_readers import AnalogMultiChannelReader
-    from nidaqmx.errors import DaqError
-
-    system = nidaqmx.system.System.local()
-else:
-    raise NotImplementedError('NIDAQ only available on Windows.')
+import nidaqmx
+import nidaqmx.system
+from nidaqmx.constants import AcquisitionType, TerminalConfiguration
+from nidaqmx.stream_readers import AnalogMultiChannelReader
+from nidaqmx.errors import DaqError
+system = nidaqmx.system.System.local()
 
 
 class ForceTransducers(BaseInput):
     """1D transducers."""
+    name = 'ToonTransducers'
+    type = 'ForceTransducers'
+    channel_count = 10
 
     def __init__(self, **kwargs):
-
-        BaseInput.__init__(self, **kwargs)
-
+        super(ForceTransducers, self).__init__(**kwargs)
         self._device_name = system.devices[0].name  # Assume the first NIDAQ-mx device is the one we want
         self._channels = [self._device_name + '/ai' + str(n) for n in
                           [2, 9, 1, 8, 0, 10, 3, 11, 4, 12]]
         self._data_buffer = np.full(10, np.nan)
 
     def __enter__(self):
+        super(ForceTransducers, self).__enter__()
         self._device = nidaqmx.Task()
-
         self._device.ai_channels.add_ai_voltage_chan(
             ','.join(self._channels),
             terminal_config=TerminalConfiguration.RSE
         )
-
         self._device.timing.cfg_samp_clk_timing(200, sample_mode=AcquisitionType.CONTINUOUS,
-                                                samps_per_chan=10)
-        self._reader = AnalogMultiChannelReader(self._device.in_stream)
+                                                samps_per_chan=1)
+
+        def callback(task_handle, every_n_samples_type, number_of_samples, callback_data):
+            ts = self.time()
+            samples = self._device.read(number_of_samples_per_channel=1)
+            self.outlet.push_sample(samples, ts)
+            return 0
+
+        self._device.register_every_n_samples_acquired_into_buffer_event(1, callback)
         self._device.start()
         return self
 
     def read(self):
-        try:
-            self._reader.read_one_sample(self._data_buffer, timeout=0)
-            timestamp = self.time()
-        except DaqError:
-            return None
-        return {'time': timestamp, 'data': np.copy(self._data_buffer)}
+        pass
 
     def __exit__(self, type, value, traceback):
         self._device.stop()
