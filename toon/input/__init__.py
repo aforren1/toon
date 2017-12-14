dfc916acc826b41939ef2446ab6729d22f1ed578
Alex Forrence <aforren1@jhu.edu>
2017-12-14 14:58:30 -0500

    Nix 2.7 testing for now, conditionally import force transducers

--- toon/input/__init__.py
+++ toon/input/__init__.py
@@ -1,4 +1,7 @@
-from .force_transducers import ForceTransducers
+import os
+
 from .hand import Hand
 from .keyboard import Keyboard
 from .mouse import Mouse
+if os.name == 'nt':
+    from .force_transducers import ForceTransducers
