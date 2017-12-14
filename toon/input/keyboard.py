5f8c1d84813ed8075794f01ab4ef5ae4d37dfeb8
Alex Forrence <aforren1@jhu.edu>
2017-12-14 14:04:20 -0500

    Add keyboard

--- toon/input/keyboard.py
+++ toon/input/keyboard.py
@@ -1,35 +1,73 @@
-from toon.input.lsl_wrapper import BaseInput
+from toon.input.base_input import BaseInput
 from pynput import keyboard
+from ctypes import c_bool, c_uint16, c_char
+
 
 class Keyboard(BaseInput):
-    name = 'ToonKeyboard'
-    type = 'Keyboard'
-    channel_count = 2
-    def __init__(self, keys=None, **kwargs):
-        self._keys = keys
-        super(Keyboard, self).__init__(channel_format='int32', **kwargs)
+    def samp_freq(**kwargs):
+        return kwargs.get('sampling_frequency', 100)
+
+    def data_shapes(**kwargs):
+        return [[1], [1], [1]]
+
+    def data_types(**kwargs):
+        return [c_bool, c_uint16, c_char]
+
+    def __init__(self, keys=None, sampling_frequency=100, **kwargs):
+        self.keys = keys
+        super(Keyboard, self).__init__(sampling_frequency=sampling_frequency, **kwargs)
+
     def __enter__(self):
-        super(Keyboard, self).__enter__()
         self._on = []
+        self.pressed = []
+        self.index = []
+        self.char = []
+        self.times = []
         self.dev = keyboard.Listener(on_press=self.on_press,
                                      on_release=self.on_release)
         self.dev.start()
         self.dev.wait()
         return self
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.dev.stop()
         self.dev.join()
+
+    def read(self):
+        if not self.times:
+            return None, None
+        time2 = []
+        pressed2 = []
+        index2 = []
+        char2 = []
+        time2[:] = self.times
+        pressed2[:] = self.pressed
+        index2[:] = self.index
+        char2[:] = self.char
+        self.times = []
+        self.pressed = []
+        self.index = []
+        self.char = []
+        return time2[-1], [pressed2[-1], index2[-1], char2[-1]]
+
     def on_press(self, key):
-        ts = self.time()
+        ts = self.clock()
         if not isinstance(key, keyboard.Key):
-            if key.char in self._keys and key.char not in self._on:
-                index = self._keys.index(key.char)
-                self.outlet.push_sample([index, 1], ts)
+            if key.char in self.keys and key.char not in self._on:
+                index = self.keys.index(key.char)
+                self.pressed.append(True)
+                self.index.append(index)
+                self.char.append(key.char)
+                self.times.append(ts)
                 self._on.append(key.char)
+
     def on_release(self, key):
-        ts = self.time()
+        ts = self.clock()
         if not isinstance(key, keyboard.Key):
-            if key.char in self._keys and key.char in self._on:
-                index = self._keys.index(key.char)
-                self.outlet.push_sample([index, 0], ts)
+            if key.char in self.keys and key.char in self._on:
+                index = self.keys.index(key.char)
+                self.pressed.append(False)
+                self.index.append(index)
+                self.char.append(key.char)
+                self.times.append(ts)
                 self._on.remove(key.char)
--- toon/tests/test_input.py
+++ toon/tests/test_input.py
@@ -1,5 +1,6 @@
-from toon.input.mp_input import MultiprocessInput as MPI
+from toon.input.mp_input import MultiprocessInput as MpI
 from toon.input.mouse import Mouse
+from toon.input.keyboard import Keyboard
 from toon.input.fake import FakeInput
 from timeit import default_timer
 import numpy as np
@@ -7,17 +8,15 @@ import ctypes
 import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=5, suppress=True)
-#dev = MPI(Mouse)
-dev = MPI(FakeInput, sampling_frequency=1000, data_shape=[[5]],
-          data_type=[ctypes.c_double])
+# dev = MpI(Mouse)
+dev = MpI(Keyboard, keys=['a', 's', 'd', 'f'])
+# dev = MPI(FakeInput, sampling_frequency=10000, data_shape=[[5]], data_type=[ctypes.c_double])
 
 read_times = []
-num_samples = []
-last_sample_and_ret = []
 diffs = []
 with dev as d:
     t0 = default_timer()
-    t1 = t0 + 10
+    t1 = t0 + 5
     t2 = 0
     while default_timer() < t1:
         t0 = default_timer()
@@ -25,9 +24,7 @@ with dev as d:
         t3 = default_timer()
         read_times.append(t3 - t0)
         if time is not None:
-            num_samples.append(len(time))
-            last_sample_and_ret.append(t3 - time[-1])
-            print(data)
+            print(time)
             diffs.extend(np.diff(time))
         while default_timer() < t2:
             pass
@@ -35,7 +32,5 @@ with dev as d:
 
 plt.plot(read_times)
 plt.show()
-plt.plot(num_samples)
-plt.show()
-plt.plot(last_sample_and_ret)
+plt.plot(diffs)
 plt.show()
