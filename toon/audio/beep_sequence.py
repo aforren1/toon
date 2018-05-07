76d3b073e1695a4d3aaeff3915dc036696b11963
Alex Forrence <aforren1@jhu.edu>
2018-05-07 14:02:40 -0400

    Usability for metronome generation, cleaning up the HAND, and version bump

--- toon/audio/beep_sequence.py
+++ toon/audio/beep_sequence.py
@@ -1,3 +1,4 @@
+from numbers import Real
 import numpy as np
 
 
@@ -34,40 +35,35 @@ def apply_hanning(sample, sample_rate):
 
 def beep_sequence(click_freq=(440, 660, 880, 1220),
                   inter_click_interval=0.5,
-                  num_clicks=4,
                   dur_clicks=0.04,
+                  lead_in=0.1,
                   sample_rate=44100,
                   stereo=True):
     """Generate a series of sine waves, similar to a metronome.
 
     Kwargs:
-        click_freq (list, tuple, or 1d numpy array): The frequency of each beep.
+        click_freq (number, list, tuple, or 1d numpy array): The frequency of each beep.
         inter_click_interval (int or float): The period between the beep midpoints.
-        num_clicks (int): Number of clicks.
         dur_clicks (int or float): Float or int, duration of each beep in seconds.
+        lead_in (float): Amount of time in seconds before the center of the first beep.
         sample_rate (int or float): Sampling rate for the wave.
         stereo (bool): Generate audio for two channels.
 
     Returns:
-        A 1-dimensional numpy array.
-
-    Notes:
-        There's a 100 millisecond pause before the *center* of the first beep.
+        A numpy array, 1-dimensional if mono, 2-dimensional (n by 2) if stereo.
 
     Example:
         To generate four beeps of different frequency and spaced by half a second,
 
         >>> my_train = beep_sequence([1220, 400, 410, 620], inter_click_interval=0.5)
     """
-    if len(click_freq) != 1 and len(click_freq) != num_clicks:
-        raise ValueError('click_freq must be either 1 or match the num_clicks.')
-    if len(click_freq) == 1:
-        click_freq = click_freq * num_clicks
+    if isinstance(click_freq, Real):
+        click_freq = [click_freq]
     beeps = [beep(n, duration=dur_clicks, sample_rate=sample_rate) for n in click_freq]
     space = np.zeros(int((inter_click_interval * sample_rate) - len(beeps[0])))
-    out = np.zeros((int(sample_rate * 0.1 - len(beeps[0]) / 2)))
+    out = np.zeros((int(sample_rate * lead_in - len(beeps[0]) / 2)))
     out = np.append(out, beeps[0])
-    for i in range(num_clicks - 1):
+    for i in range(len(click_freq) - 1):
         out = np.append(out, space)
         out = np.append(out, beeps[i + 1])
     if stereo:
--- toon/input/hand.py
+++ toon/input/hand.py
@@ -1,8 +1,9 @@
 import struct
-import numpy as np
-import hid
-from toon.input.base_input import BaseInput
 from ctypes import c_double
+
+import hid
+import numpy as np
+from toon.input.base_input import BaseInput
 
 
 class Hand(BaseInput):
@@ -22,16 +23,15 @@ class Hand(BaseInput):
 
     def __init__(self, nonblocking=False, **kwargs):
         super(Hand, self).__init__(**kwargs)
-        self._rot = np.pi / 4.0
-        self._sinrot = np.sin(self._rot)
-        self._cosrot = np.cos(self._rot)
+        self._sqrt2 = np.sqrt(2)
         self.nonblocking = nonblocking
         self._device = None
         self._data_buffer = np.full(15, np.nan)
 
     def __enter__(self):
         self._device = hid.device()
-        dev_path = next((dev for dev in hid.enumerate() if dev['vendor_id'] == 0x16c0 and dev['interface_number'] == 0), None)['path']
+        dev_path = next((dev for dev in hid.enumerate()
+                         if dev['vendor_id'] == 0x16c0 and dev['interface_number'] == 0), None)['path']
         self._device.open_path(dev_path)
         self._device.set_nonblocking(self.nonblocking)
         return self
@@ -43,11 +43,12 @@ class Hand(BaseInput):
         data = self._device.read(46)
         time = self.clock()
         if data:
-            data = struct.unpack('>Lh' + 'H'*20, bytearray(data)) # Timestamp, deviation, and 20 unsigned ints
+            # Timestamp, deviation, and 20 unsigned ints
+            data = struct.unpack('>Lh' + 'H'*20, bytearray(data))
             data = np.array(data, dtype='d')
             data[0] /= 1000.0
             data[2:] /= 65535.0
-            self._data_buffer[0::3] = data[2::4] * self._cosrot - data[3::4] * self._sinrot  # x
-            self._data_buffer[1::3] = data[2::4] * self._sinrot + data[3::4] * self._cosrot  # y
+            self._data_buffer[0::3] = (data[2::4] - data[3::4])/self._sqrt2  # x
+            self._data_buffer[1::3] = (data[2::4] + data[3::4])/self._sqrt2  # y
             self._data_buffer[2::3] = data[4::4] + data[5::4]  # z
             return time, self._data_buffer
