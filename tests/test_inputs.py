7d1b1ffcce1bce525720ccefb196cd55b0710717
Alex Forrence <aforren1@jhu.edu>
2018-05-24 14:00:09 -0400

    Test

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -85,6 +85,7 @@ class TestMpInput(TestCase):
         self.assertEqual(data.shape, (2, 5))
         diff = np.diff(time)
         print(time)
+        print(data)
         self.assertTrue(np.isclose(diff, 0.001, atol=1e-3))
 
     def test_multi_devices(self):
