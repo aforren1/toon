8de2dd52b35b6a43e421721721940098d0272b50
Alex Forrence <aforren1@jhu.edu>
2018-11-19 09:26:51 -0500

    Prettier display

--- demos/mouse.py
+++ demos/mouse.py
@@ -4,13 +4,15 @@ from psychopy import visual, core, event
 from toon.input.mpdevice import MpDevice
 from toon.input.mouse import Mouse
 
-win = visual.Window(fullscr=True, units='pix')
+win = visual.Window(fullscr=True, units='pix',
+                    waitBlanking=False, multiSample=True,
+                    numSamples=8)
 
 psy_mouse = event.Mouse()
 toon_mouse = MpDevice(Mouse)
 
-psy_cir = visual.Circle(win, size=100, fillColor='red', opacity=0.5)
-toon_cir = visual.Circle(win, size=100, fillColor='blue', opacity=0.5)
+psy_cir = visual.Circle(win, size=100, fillColor='red', opacity=0.5, lineColor=None)
+toon_cir = visual.Circle(win, size=100, fillColor='blue', opacity=0.5, lineColor=None)
 toon_cir.pos = (-1920/2, 1080/2)
 with toon_mouse:  # alternatively, use toon_mouse.start(), toon_mouse.stop()
     while not event.getKeys(['esc', 'escape']):
