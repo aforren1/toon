5517b886d9a802ab46ccd925dc8dc7d34637db4f
Alex Forrence <aforren1@jhu.edu>
2018-11-30 17:14:55 -0500

    Notes

--- toon/input/new_obs.py
+++ toon/input/new_obs.py
@@ -4,9 +4,6 @@ import numpy as np
 # https://stackoverflow.com/questions/31282764/when-subclassing-a-numpy-ndarray-how-can-i-modify-getitem-properly
 # and https://docs.scipy.org/doc/numpy/user/basics.subclassing.html
 
-# copy, empty_like, etc. don't *quite* get the timestamp right
-# they only get the last element AFAIK
-
 
 class Obs(np.ndarray):
     def __new__(cls, data, time=None):
@@ -34,3 +31,6 @@ class Obs(np.ndarray):
 if __name__ == '__main__':
     xx = Obs([[3], [2], [3]], time=[0.1, 0.2, 0.3])
     yy = Obs([1., 3.14, 2], time=1.2)
+    zz = xx[:2].copy()  # np.copy(arr) loses the time attribute
+    zz == xx[:2]
+    zz.time == xx[:2].time
