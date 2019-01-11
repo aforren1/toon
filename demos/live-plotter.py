c0b57850b40b10031917489b436592345d9006aa
Alex Forrence <aforren1@jhu.edu>
2019-01-11 17:34:56 -0500

    A better cyberglove

--- demos/live-plotter.py
+++ demos/live-plotter.py
@@ -2,7 +2,7 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 from toon.input import MpDevice
-from toon.input.mouse import Mouse
+from toon.input.cyberglove import Cyberglove
 
 
 class LivePlot(pg.GraphicsLayoutWidget):
@@ -10,26 +10,27 @@ class LivePlot(pg.GraphicsLayoutWidget):
         super(LivePlot, self).__init__()
         self.plot = self.addPlot()
         self.curves = []
-        self.device = MpDevice(Mouse)
+        self.device = MpDevice(Cyberglove, port='/dev/ttyUSB0')
         self.current_data = None
-        for i in range(2):
-            color = pg.intColor(i, hues=2, alpha=255, width=3)
+        for i in range(18):
+            color = pg.intColor(i, hues=18, alpha=255, width=3)
             self.curves.append(self.plot.plot((0, 0), pen=pg.mkPen(color=color)))
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
         self.timer.start(0)
 
     def update(self):
-        clicks, pos, scroll = self.device.read()
-        if pos is None:
+        dat = self.device.read()
+        print(np.diff(dat.time))
+        if dat is None:
             return
         if self.current_data is None:
-            self.current_data = pos
-        elif self.current_data.shape[0] < 1000:
-            self.current_data = np.vstack((self.current_data, pos))
+            self.current_data = dat
+        elif self.current_data.shape[0] < 500:
+            self.current_data = np.vstack((self.current_data, dat))
         else:
-            self.current_data = np.roll(self.current_data, -pos.shape[0], axis=0)
-            self.current_data[-pos.shape[0]:, :] = pos
+            self.current_data = np.roll(self.current_data, -dat.shape[0], axis=0)
+            self.current_data[-dat.shape[0]:, :] = dat
         for counter, c in enumerate(self.curves):
             c.setData(y=self.current_data[:, counter])
 
