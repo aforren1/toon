2511e72360ae8a89607ac65f06a6cff0d14bdbba
Alex Forrence <aforren1@jhu.edu>
2019-01-14 16:55:59 -0500

    Revert back to Mouse for plot demo

--- demos/live-plotter.py
+++ demos/live-plotter.py
@@ -2,7 +2,7 @@ import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 from toon.input import MpDevice
-from toon.input.cyberglove import Cyberglove
+from toon.input.mouse import Mouse
 
 
 class LivePlot(pg.GraphicsLayoutWidget):
@@ -10,10 +10,10 @@ class LivePlot(pg.GraphicsLayoutWidget):
         super(LivePlot, self).__init__()
         self.plot = self.addPlot()
         self.curves = []
-        self.device = MpDevice(Cyberglove, port='/dev/ttyUSB0')
+        self.device = MpDevice(Mouse)
         self.current_data = None
-        for i in range(18):
-            color = pg.intColor(i, hues=18, alpha=255, width=3)
+        for i in range(2):
+            color = pg.intColor(i, hues=2, alpha=255, width=3)
             self.curves.append(self.plot.plot((0, 0), pen=pg.mkPen(color=color)))
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
@@ -21,16 +21,16 @@ class LivePlot(pg.GraphicsLayoutWidget):
         self.playing = True
 
     def update(self):
-        dat = self.device.read()
-        if dat is None:
+        clicks, pos, scroll = self.device.read()
+        if pos is None:
             return
         if self.current_data is None:
-            self.current_data = dat
+            self.current_data = pos
         elif self.current_data.shape[0] < 500:
-            self.current_data = np.vstack((self.current_data, dat))
+            self.current_data = np.vstack((self.current_data, pos))
         else:
-            self.current_data = np.roll(self.current_data, -dat.shape[0], axis=0)
-            self.current_data[-dat.shape[0]:, :] = dat
+            self.current_data = np.roll(self.current_data, -pos.shape[0], axis=0)
+            self.current_data[-pos.shape[0]:, :] = pos
         if self.playing:
             for counter, c in enumerate(self.curves):
                 c.setData(y=self.current_data[:, counter])
