feae17fd029382a7590fe3f3e13d38c68cfdb3c3
Alex Forrence <aforren1@jhu.edu>
2019-01-18 11:38:44 -0500

    Make make_obs more visible, update benchmark utility (one at a time so far)

--- demos/bench.py
+++ demos/bench.py
@@ -1,47 +1,56 @@
-from toon.input.device import BaseDevice, make_obs
-from ctypes import c_double
 from timeit import default_timer
-from functools import reduce
-from operator import mul
-# 3 dims: sampling frequency, volume (e.g. high-dim # obs), num of unique Obs
+from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
+from toon.input import BaseDevice, MpDevice, make_obs
 
 
-class Dummy(BaseDevice):
-    counter = 0
-    t0 = default_timer()
-    Num = make_obs('Num', (128,), c_double)
+class TestDevice(BaseDevice):
+    def __init__(self, obses, device_sampling_freq):
+        for count, val in enumerate(obses):
+            setattr(self, 'Obs%s' % count, val)
+        self.obses = obses
+        self.device_sampling_freq = device_sampling_freq
+        self.t0 = default_timer()
+        super(TestDevice, self).__init__()
 
     def read(self):
-        while default_timer() - self.t0 < (1.0/self.sampling_frequency):
+        data = []
+        while default_timer() - self.t0 < (1.0/self.device_sampling_freq):
             pass
         self.t0 = default_timer()
-        t = self.clock()
-        self.counter += 1
-        dim = reduce(mul, self.Num.shape, 1)
-        return self.Returns(self.Num(t, [self.counter] * dim))
+        for i in self.obses:
+            data.append(i(self.clock(), np.random.random(i.shape)))
+        return data
 
 
 if __name__ == '__main__':
-    from time import sleep
-    import matplotlib.pyplot as plt
-    from statistics import median
-    from toon.input.mpdevice import MpDevice
+    # user-side sampling period: 1/144, 1/60, 1/10, 1
+    # device sampling freq: 10, 100, 1000, 5000
+    # number of Obs: 1, 2, 5, 10
+    # dims of Obs: (1,), (10,), (100,), (10, 10)
+    # buffer size: 1, 10, 100, 1000
+    user_sampling_period = 1.0/60
+    device_sampling_freq = 1000
+    number_of_obs = 1
+    obs_dims = (1000,)
+    buffer_size = 100
+    n_samples = 1000
 
-    Dummy.sampling_frequency = 1000
-    dev = MpDevice(Dummy())
+    obses = []
+    for i in range(number_of_obs):
+        obses.append(make_obs('Obs%s' % i, obs_dims, float))
 
-    vals = []
-
+    times = []
+    dev = MpDevice(TestDevice(obses, device_sampling_freq), buffer_len=buffer_size)
     with dev:
-        t0 = default_timer()
-        while default_timer() - t0 < 15:
-            t1 = default_timer()
-            dat = dev.read()
-            if dat is not None:
-                vals.append(default_timer() - t1)
-                sleep(0.016)
-    vals = vals[10:]  # first couple are pathological
-    print('Worst case: %f' % max(vals))
-    print('Median: %f' % median(vals))
-    plt.plot(vals)
+        for i in range(n_samples):
+            t0 = default_timer()
+            res = dev.read()
+            t1 = default_timer() - t0
+            times.append(t1)
+            sleep(user_sampling_period)
+    print('Worst: %f' % np.max(times))
+    print('Median: %f' % np.median(times))
+    plt.plot(times)
     plt.show()
