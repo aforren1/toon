02cd72d5d9f6bb28b193840bbb0f1efefb39c285
Alex Forrence <alex.forrence@gmail.com>
2020-09-04 13:26:26 -0400

    Update build process (py38, new cibuildwheel)

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='toon',
-    version='0.15.8a2',
+    version='0.15.8a3',
     description='Tools for neuroscience experiments',
     long_description=desc,
     long_description_content_type='text/markdown',
