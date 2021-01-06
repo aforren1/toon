03ccda2ab892be070dea490bfac021d7bce9cf2c
Alex Forrence <alex.forrence@gmail.com>
2021-01-06 13:20:40 -0500

    0.15.9

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='toon',
-    version='0.15.9a2',
+    version='0.15.9',
     description='Tools for neuroscience experiments',
     long_description=desc,
     long_description_content_type='text/markdown',
@@ -60,7 +60,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=requirements,
     keywords='psychophysics neuroscience input experiment',
